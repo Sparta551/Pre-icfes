788c021900d4528cbf2d6c5000292c6d581d14fc
Juanfft01 <169065814+Juanfft01@users.noreply.github.com>
2024-09-13 09:02:36 -0500

    Add files via upload

---
+++ Individual/Juan Felipe Fajardo/Avance en clase.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE biblioteca;
+use biblioteca;
+CREATE TABLE libro(
+idLibro varchar(20) primary key ,
+tituloLibro varchar(50) not null,
+identificacionAutorFK varchar(50),
+anoPublicacion year not null,
+estadoLibro bool
+);
+
+CREATE TABLE Autor(
+identificacionAutor varchar(20) primary key ,
+nombreAutor varchar(20) not null,
+fechaNacimiento date
+);
+
+CREATE TABLE Miembro(
+identificacionMiembro int AUTO_INCREMENT primary key ,
+nombreMiembro varchar(20) not null,
+direccionMiembro varchar(50),
+fechaInscripcion date not null,
+estadoMiembro bool
+);
+
+CREATE TABLE Prestamo(
+codigoPrestamo int AUTO_INCREMENT primary key ,
+fechaPrestamo date not null,
+fechaDevolucion date,
+idLibroFK varchar(20) not null,
+idMiembroFK varchar(20) not null,
+estadoPrestamo bool
+);
+
+alter table Libro
+add constraint FKAutorLibro
+foreign key (identificacionAutorFK)
+references Autor(identificacionAutor);
