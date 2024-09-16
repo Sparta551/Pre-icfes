316fcf0a3d4c3875fe7727fa38cd5c06fa129447
Juanfft01 <169065814+Juanfft01@users.noreply.github.com>
2024-09-16 00:03:49 -0500

    Update Avance en clase.sql

--- Individual/Juan Felipe Fajardo/Avance en clase.sql
+++ Individual/Juan Felipe Fajardo/Avance en clase.sql
@@ -5,7 +5,7 @@ idLibro varchar(20) primary key ,
 tituloLibro varchar(50) not null,
 identificacionAutorFK varchar(50),
 anoPublicacion year not null,
-estadoLibro bool
+estadoLibro boolean
 );
 
 CREATE TABLE Autor(
@@ -19,19 +19,29 @@ identificacionMiembro varchar(20) primary key ,
 nombreMiembro varchar(20) not null,
 direccionMiembro varchar(50),
 fechaInscripcion date not null,
-estadoMiembro bool
+estadoMiembro boolean
 );
 
 CREATE TABLE Prestamo(
 codigoPrestamo int AUTO_INCREMENT primary key ,
 fechaPrestamo date not null,
 fechaDevolucion date,
-idLibroFK varchar(20) not null,
-idMiembroFK varchar(20) not null,
-estadoPrestamo bool
+idLibroFK varchar(20),
+idMiembroFK varchar(20),
+estadoPrestamo boolean
 );
 
 alter table Libro
 add constraint FKAutorLibro
 foreign key (identificacionAutorFK)
 references Autor(identificacionAutor);
+
+alter table Prestamo
+add constraint FKMiembroPrestamo
+foreign key (idMiembroFK)
+references Miembro(identificacionMiembro);
+
+alter table Prestamo
+add constraint FKLibroPrestamo
+foreign key (idLibroFK)
+references Libro(idLibro);
