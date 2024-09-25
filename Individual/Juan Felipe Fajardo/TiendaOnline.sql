8af737dddc2770b7e9631be7a88cb0982efb2d93
Juanfft01 <169065814+Juanfft01@users.noreply.github.com>
2024-09-24 21:37:23 -0500

    Update TiendaOnline.sql
    Se terminaron las consultas,  falta el avg y el otro (no se cuales son)

--- Individual/Juan Felipe Fajardo/TiendaOnline.sql
+++ Individual/Juan Felipe Fajardo/TiendaOnline.sql
@@ -30,7 +30,7 @@ create table ventas(
 codigoVenta int auto_increment primary key,
 usuarioFK varchar(15),
 clienteFK int,
-diaVenta date
+fechaVenta date
 );
 
 create table venta_producto(
@@ -61,8 +61,9 @@ foreign key (productoFK)
 references producto(codigoProducto);
 
 alter table usuario add genero varchar(5);
-alter table ventas drop diaVenta;
-insert into producto values ('','Chorizo','cochinera',4,5000),('','mazorca','chicken little', 2, 3000),('','leche','Halpina',1,4000),('','huevos','MyHuevos',12,14000);
-insert into clientes values ('','Mauricio','145550287',1984-10-01),('','Andres','13657893',2001-12-15),('','Compae','1134780185',1999-8-27),('','Laura','101567328',2005-3-05);
+
+insert into producto values ('','Chorizo','cochinera',4,5000),('','mazorca','chicken little', 2, 3000),('','leche','Halpina',1,4000),('','huevos','MyHuevos',12,15000);
+insert into clientes values ('','Mauricio','145550287','1984-10-01'),('','Andres','13657893','2001-12-15'),('','Compae','1134780185','1999-01-27'),('','Laura','101567328','2005-03-05');
 insert into usuario values ('1234567890','Carol','3068970213','Colombia','Empleado','F'),('0987654321','Rex','3067891234','Suba','Empleado','M'),('1325498760','Harold','3057894632','Colombia','Empleado','M'),('8976345017','Cata','3108036422','Peru','Administrador','F');
-insert into ventas values ('',1234567890,1),('',8976345017,2);
+insert into ventas values ('','1234567890',1,'2023-03-15'),('','8976345017',2,'2024-06-24'),('','8976345017',4,'2023-06-04'),('','0987654321',3,'2024-02-18');
+insert into venta_producto values (1,1,3,15000),(2,4,2,30000),(3,2,1,3000),(4,3,3,12000);
