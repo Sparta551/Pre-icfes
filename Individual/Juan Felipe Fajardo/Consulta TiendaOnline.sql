b3c39673bcf309c87d2ffa5c2d5b7c30919cfe8d
Juanfft01 <169065814+Juanfft01@users.noreply.github.com>
2024-09-24 21:38:45 -0500

    Update Consulta TiendaOnline.sql
    ACA se terminaron las consultas (lo puse en el que no era xD)

--- Individual/Juan Felipe Fajardo/Consulta TiendaOnline.sql
+++ Individual/Juan Felipe Fajardo/Consulta TiendaOnline.sql
@@ -5,10 +5,12 @@ select * from Ventas;
 select * from Usuario;
 select * from Clientes;
 
-
-
 select nombreProducto as 'Producto' from producto order by precio asc;
+select nombreCliente as 'Nombre', cedulaCliente 'Cedula' from clientes where month(nacimientoCliente) = 01;
 select * from usuario where rol='Empleado';
+select usuarioFK as 'Vendedor', fechaVenta 'Fecha' from ventas where month(fechaVenta) = 03 or month(fechaVenta) = 06;
+select nombreProducto as Producto, marca from producto where nombreProducto like '%r%';
+select ventaFK as 'codigo de venta' from venta_producto where (total/cantidadProducto) = 15000 or (total/cantidadProducto) = 30000;
 
 select sum(Total) from venta_producto;
 select min(Total) from venta_producto;
