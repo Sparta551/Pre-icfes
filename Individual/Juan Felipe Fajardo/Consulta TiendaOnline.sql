461ac2f6051dfb139fb66b7dea6d3f15ca7d609f
Juanfft01 <169065814+Juanfft01@users.noreply.github.com>
2024-09-30 08:40:55 -0500

    Add files via upload

--- Individual/Juan Felipe Fajardo/Consulta TiendaOnline.sql
+++ Individual/Juan Felipe Fajardo/Consulta TiendaOnline.sql
@@ -38,3 +38,36 @@ select nombreCliente, total
 from ventas v 
 inner join clientes c on c.codigoCliente=v.clienteFK
 inner join venta_producto vp on vp.ventaFK=v.codigoVenta where (select max(total) from venta_producto)=vp.total;
+
+select * from clientes where codigoCliente=2;
+
+delimiter //
+create procedure registro_usuario(cedula varchar(15),nombre varchar(20),telefono int, direccion varchar(20),rol varchar(20), genero varchar(5))
+begin
+insert into usuario values (cedula,nombre,telefono,direccion,rol,genero);
+end//
+
+create procedure registro_producto(codigo int,nombre varchar(15),marca varchar(15),cantidad int, precio int)
+begin
+insert into producto values (codigo,nombre,marca,cantidad,precio);
+end//
+
+create procedure update_precio (price int, id int)
+begin
+update Producto set precio=price where codigoProducto=id;
+end//
+delimiter ;
+
+call update_precio(4500,3);
+
+call registro_usuario;
+
+create view consultarCliente as select codigoCliente,nombreCliente, apellido from clientes;
+
+select apellido from consultarCliente;
+
+#pocedimiento para inactivar un cliente
+#procedimiento para consultar los productos que ha comprado un cliente
+#procedimiento para modificar la fecha de nacimiento
+#crear vista que consulte que cliente compro un producto y su numero de orden
+#crear vista que muestre el cliente que mas compras haya hecho
